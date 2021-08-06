6a4016a5f440189e6728a296e76edcd6632a0c99
Ea0011 <avagyanee@gmail.com>
2021-08-07 01:48:12 +0400

    WIP(Autoencoder): add initial structure to module

--- src/models/autoencoder.py
+++ src/models/autoencoder.py
@@ -22,24 +22,67 @@ class CosineSimilarityLoss(nn.Module):
 
 
 class AutoEncoder(pl.LightningModule):
-  def __init__(self, hparams) -> None:
+  def __init__(self, hparams, input_dim) -> None:
     super().__init__()
     self.save_hyperparameters('hparams')
+    self.input_dim = input_dim
+    self.cosine_loss = CosineSimilarityLoss()
   
   def prepare_encoder(self):
-    pass
+    self.encoder = nn.Sequential(
+      nn.Linear(self.input_dim, 256),
+      nn.ReLU(),
+      nn.Linear(256, 16)
+    )
+
+    return self.encoder
 
   def prepare_decoder(self):
-    pass
+    self.decoder = nn.Sequential(
+      nn.Linear(16, 256),
+      nn.ReLU(),
+      nn.Linear(256, 1024)
+    )
+
+    return self.decoder
+
+  def forward(self, x):
+    z = self.encoder(x)
+    return z
+
+  def general_step(self, x, y):
+    x = x.view(x.size(0), -1)
+    z = self.encoder(x)
+    cosine_loss = self.cosine_loss(z, y)
+
+    x_hat = self.decoder(z)
+    reconstruction_loss = F.l1_loss(x, x_hat)
+
+    return (
+      self.hparams["cos_loss_weight"] * cosine_loss,
+      self.hparams["rec_loss_weight"] * reconstruction_loss,
+      self.hparams["cos_loss_weight"] * cosine_loss + self.hparams["rec_loss_weight"] * reconstruction_loss
+    )
 
   def training_step(self, batch, batch_idx):
-    pass
+    x, y = batch
+    (_, _, train_loss) = self.general_step(x, y)
 
-  def validation_setp(self, batch, batch_idx):
-    pass
+    self.log("train_loss", train_loss)
+
+    return train_loss
+
+  def validation_step(self, batch, batch_idx):
+    x, y = batch
+    (_, _, val_loss) = self.general_step(x, y)
+
+    self.log("val_loss", val_loss)
+
+    return val_loss
 
   def test_step(self, batch, batch_idx):
     pass
 
   def configure_optimizers(self):
-    pass
+    optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams["learning_rate"])
+    return optimizer
